4996d52cd47c23f7f163cb2c941ddfa9a9a63d6d
netravler <pheintz@hotmail.com>
2018-09-13 14:03:15 -0500

    Add files via upload

---
+++ Check_tracker_pos/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace Check_tracker_pos
+{
+    partial class check_tracker_pos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnsubtract = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnTax1 = new System.Windows.Forms.Button();
+            this.btnTax2 = new System.Windows.Forms.Button();
+            this.btnTax3 = new System.Windows.Forms.Button();
+            this.btnNoTax = new System.Windows.Forms.Button();
+            this.btnRepeat = new System.Windows.Forms.Button();
+            this.lblCurrentTrans = new System.Windows.Forms.Label();
+            this.lblTransHistory = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(727, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Location = new System.Drawing.Point(785, 404);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(49, 43);
+            this.btnTwo.TabIndex = 1;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            // 
+            // btnThree
+            // 
+            this.btnThree.Location = new System.Drawing.Point(843, 404);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(49, 43);
+            this.btnThree.TabIndex = 2;
+            this.btnThree.Text = "3";
+            this.btnThree.UseVisualStyleBackColor = true;
+            // 
+            // btnFour
+            // 
+            this.btnFour.Location = new System.Drawing.Point(727, 453);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(49, 43);
+            this.btnFour.TabIndex = 3;
+            this.btnFour.Text = "4";
+            this.btnFour.UseVisualStyleBackColor = true;
+            // 
+            // btnFive
+            // 
+            this.btnFive.Location = new System.Drawing.Point(785, 453);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(49, 43);
+            this.btnFive.TabIndex = 4;
+            this.btnFive.Text = "5";
+            this.btnFive.UseVisualStyleBackColor = true;
+            // 
+            // btnSix
+            // 
+            this.btnSix.Location = new System.Drawing.Point(843, 453);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(49, 43);
+            this.btnSix.TabIndex = 5;
+            this.btnSix.Text = "6";
+            this.btnSix.UseVisualStyleBackColor = true;
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.Location = new System.Drawing.Point(727, 502);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(49, 43);
+            this.btnSeven.TabIndex = 6;
+            this.btnSeven.Text = "7";
+            this.btnSeven.UseVisualStyleBackColor = true;
+            // 
+            // btnEight
+            // 
+            this.btnEight.Location = new System.Drawing.Point(785, 502);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(49, 43);
+            this.btnEight.TabIndex = 7;
+            this.btnEight.Text = "8";
+            this.btnEight.UseVisualStyleBackColor = true;
+            // 
+            // btnNine
+            // 
+            this.btnNine.Location = new System.Drawing.Point(843, 502);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(49, 43);
+            this.btnNine.TabIndex = 8;
+            this.btnNine.Text = "9";
+            this.btnNine.UseVisualStyleBackColor = true;
+            // 
+            // btnOne
+            // 
+            this.btnOne.Location = new System.Drawing.Point(727, 404);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(49, 43);
+            this.btnOne.TabIndex = 9;
+            this.btnOne.Text = "1";
+            this.btnOne.UseVisualStyleBackColor = true;
+            // 
+            // btnZero
+            // 
+            this.btnZero.Location = new System.Drawing.Point(727, 551);
+            this.btnZero.Name = "btnZero";
+            this.btnZero.Size = new System.Drawing.Size(165, 41);
+            this.btnZero.TabIndex = 10;
+            this.btnZero.Text = "0";
+            this.btnZero.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(901, 404);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(61, 43);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnsubtract
+            // 
+            this.btnsubtract.Location = new System.Drawing.Point(901, 453);
+            this.btnsubtract.Name = "btnsubtract";
+            this.btnsubtract.Size = new System.Drawing.Size(61, 43);
+            this.btnsubtract.TabIndex = 12;
+            this.btnsubtract.Text = "-";
+            this.btnsubtract.UseVisualStyleBackColor = true;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(669, 598);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(293, 33);
+            this.btnEnter.TabIndex = 13;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            // 
+            // btnTax1
+            // 
+            this.btnTax1.Location = new System.Drawing.Point(669, 404);
+            this.btnTax1.Name = "btnTax1";
+            this.btnTax1.Size = new System.Drawing.Size(49, 43);
+            this.btnTax1.TabIndex = 16;
+            this.btnTax1.Text = "Tax 1";
+            this.btnTax1.UseVisualStyleBackColor = true;
+            // 
+            // btnTax2
+            // 
+            this.btnTax2.Location = new System.Drawing.Point(669, 451);
+            this.btnTax2.Name = "btnTax2";
+            this.btnTax2.Size = new System.Drawing.Size(49, 43);
+            this.btnTax2.TabIndex = 17;
+            this.btnTax2.Text = "Tax 2";
+            this.btnTax2.UseVisualStyleBackColor = true;
+            // 
+            // btnTax3
+            // 
+            this.btnTax3.Location = new System.Drawing.Point(669, 500);
+            this.btnTax3.Name = "btnTax3";
+            this.btnTax3.Size = new System.Drawing.Size(49, 43);
+            this.btnTax3.TabIndex = 18;
+            this.btnTax3.Text = "Tax 3";
+            this.btnTax3.UseVisualStyleBackColor = true;
+            // 
+            // btnNoTax
+            // 
+            this.btnNoTax.Location = new System.Drawing.Point(669, 549);
+            this.btnNoTax.Name = "btnNoTax";
+            this.btnNoTax.Size = new System.Drawing.Size(49, 43);
+            this.btnNoTax.TabIndex = 19;
+            this.btnNoTax.Text = "No Tax";
+            this.btnNoTax.UseVisualStyleBackColor = true;
+            // 
+            // btnRepeat
+            // 
+            this.btnRepeat.Location = new System.Drawing.Point(901, 500);
+            this.btnRepeat.Name = "btnRepeat";
+            this.btnRepeat.Size = new System.Drawing.Size(61, 90);
+            this.btnRepeat.TabIndex = 20;
+            this.btnRepeat.Text = "Repeat";
+            this.btnRepeat.UseVisualStyleBackColor = true;
+            // 
+            // lblCurrentTrans
+            // 
+            this.lblCurrentTrans.AutoSize = true;
+            this.lblCurrentTrans.Location = new System.Drawing.Point(287, 389);
+            this.lblCurrentTrans.Name = "lblCurrentTrans";
+            this.lblCurrentTrans.Size = new System.Drawing.Size(45, 13);
+            this.lblCurrentTrans.TabIndex = 22;
+            this.lblCurrentTrans.Text = "TICKET";
+            // 
+            // lblTransHistory
+            // 
+            this.lblTransHistory.AutoSize = true;
+            this.lblTransHistory.Location = new System.Drawing.Point(4, 19);
+            this.lblTransHistory.Name = "lblTransHistory";
+            this.lblTransHistory.Size = new System.Drawing.Size(98, 13);
+            this.lblTransHistory.TabIndex = 23;
+            this.lblTransHistory.Text = "Transaction History";
+            this.lblTransHistory.Click += new System.EventHandler(this.lblTransHistory_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(947, 154);
+            this.dataGridView1.TabIndex = 25;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToOrderColumns = true;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(7, 405);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(656, 226);
+            this.dataGridView2.TabIndex = 26;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToOrderColumns = true;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView3.Location = new System.Drawing.Point(7, 217);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(947, 155);
+            this.dataGridView3.TabIndex = 27;
+            this.dataGridView3.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellEndEdit);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Menu Items";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(988, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 29;
+            // 
+            // check_tracker_pos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 640);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblTransHistory);
+            this.Controls.Add(this.lblCurrentTrans);
+            this.Controls.Add(this.btnRepeat);
+            this.Controls.Add(this.btnNoTax);
+            this.Controls.Add(this.btnTax3);
+            this.Controls.Add(this.btnTax2);
+            this.Controls.Add(this.btnTax1);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnsubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnZero);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.btnNine);
+            this.Controls.Add(this.btnEight);
+            this.Controls.Add(this.btnSeven);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.btnFive);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.button1);
+            this.Name = "check_tracker_pos";
+            this.Text = "check_tracker_pos";
+            this.Load += new System.EventHandler(this.check_tracker_pos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnsubtract;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnTax1;
+        private System.Windows.Forms.Button btnTax2;
+        private System.Windows.Forms.Button btnTax3;
+        private System.Windows.Forms.Button btnNoTax;
+        private System.Windows.Forms.Button btnRepeat;
+        private System.Windows.Forms.Label lblCurrentTrans;
+        private System.Windows.Forms.Label lblTransHistory;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Check_tracker_pos/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Windows.Forms;
+
+namespace Check_tracker_pos
+{
+    public partial class check_tracker_pos : Form
+    {
+        public check_tracker_pos()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+ 
+        }
+
+        void check_tracker_pos_Load(object sender, EventArgs e)
+        {
+            // transaction history
+             DataSet ds = new DataSet("Info");
+            DataTable dt = new DataTable("Record");
+
+            string xsd = @"c:\pos\transaction.xsd";
+            string xml = @"c:\pos\transaction.xml";
+
+            if (!File.Exists(xsd))
+            {
+                dt.Columns.Add("Sequence");
+                dt.Columns.Add("TransID");
+                ds.Tables.Add(dt);
+
+                ds.WriteXmlSchema(xsd);
+                ds.WriteXml(xml);
+            }
+            else
+            {
+                ds.ReadXmlSchema(xsd);
+                ds.ReadXml(xml);
+            }
+
+            //Binds datagrid
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "TransactionHistory"; 
+            
+            // Items
+            DataSet ds1 = new DataSet("Info");
+            DataTable dt1 = new DataTable("Record");
+
+            string xsd1 = @"c:\pos\Items.xsd";
+            string xml1 = @"c:\pos\Items.xml";
+
+            if (!File.Exists(xsd))
+            {
+                dt1.Columns.Add("Sequence");
+                dt1.Columns.Add("ItemNumber");
+                ds1.Tables.Add(dt1);
+
+                ds1.WriteXmlSchema(xsd1);
+                ds1.WriteXml(xml1);
+            }
+            else
+            {
+                ds1.ReadXmlSchema(xsd1);
+                ds1.ReadXml(xml1);
+            }
+
+            //Binds datagrid
+            dataGridView3.DataSource = ds1;
+            dataGridView3.DataMember = "Items"; 
+
+            // Ticket
+            DataSet ds2 = new DataSet("Info");
+            DataTable dt2 = new DataTable("Record");
+
+            string xsd2 = @"c:\pos\Ticket.xsd";
+            //string xml2 = @"c:\pos\Items.xml";
+
+            if (!File.Exists(xsd))
+            {
+                dt2.Columns.Add("Sequence");
+                dt2.Columns.Add("ItemNumber");
+                ds2.Tables.Add(dt2);
+
+                ds2.WriteXmlSchema(xsd2);
+                //ds2.WriteXml(xml2);
+            }
+            else
+            {
+                ds2.ReadXmlSchema(xsd2);
+                //ds1.ReadXml(xml2);
+            }
+
+            //Binds datagrid
+            dataGridView2.DataSource = ds2;
+            dataGridView2.DataMember = "Ticket"; 
+            
+
+        }
+
+        private void lblTransHistory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+ 
+        }
+
+        void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            DataSet ds = (DataSet)dataGridView1.DataSource;
+            ds.WriteXml(@"c:\pos\transaction.xml", XmlWriteMode.IgnoreSchema);
+        }
+
+        void dataGridView3_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            DataSet ds1 = (DataSet)dataGridView1.DataSource;
+            ds1.WriteXml(@"c:\pos\Items.xml", XmlWriteMode.IgnoreSchema);
+        }
+    }
+}
